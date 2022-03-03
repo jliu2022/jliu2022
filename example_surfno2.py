741327caaaf168c7116c786a1acf3194afd96a78
jliu2022 <100701948+jliu2022@users.noreply.github.com>
2022-03-02 21:51:26 -0500

    Create example_surfno2.py

---
+++ example_surfno2.py
@@ -0,0 +1,9 @@
+import sys
+import os
+import numpy as np
+import datetime as dt
+import pandas as pd
+from IPython.display import HTML
+sys.path.insert(1,'MLpred/MLpred')
+import mlpred as mlpred
+import xarray as xr
